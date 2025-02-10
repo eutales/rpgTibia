11d965f5d03cf4f809871b1e9b01a9528550cc9d
tales.siqueira <tales.siqueira@PROFESSORES>
2025-02-10 15:28:48 -0300

    Adicionar arquivos de projeto.

---
+++ rpgTibia/Entities/Character.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rpgTibia.Entities
+{
+    internal class Character
+    {
+        public Character(String name)
+        {
+            this.name = name;
+        }
+
+        private String name;
+        private Vocation vocation;
+     
+        public String getName()
+            {
+                return name;
+            }
+        
+        public void atacar()
+        {
+
+        }
+
+        public void curar()
+        {
+
+        }
+
+      
+
+    }
+}
---
+++ rpgTibia/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using rpgTibia.Entities;
+
+Console.WriteLine("Hello, World!");
+
+Character character1 = new Character("Tales");
+Console.WriteLine(character1.getName());
+
+
